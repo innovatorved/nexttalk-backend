1fe078458f53cd7fbedc89c216ce7a788195c452
Ved Gupta <vedgupta@protonmail.com>
2023-02-11 16:17:40 +0530

    schema create for conversation

--- src/graphql/resolvers/conversation.ts
+++ src/graphql/resolvers/conversation.ts
@@ -1,15 +1,78 @@
 import { GraphQLContext } from "../../util/types";
+import { ApolloError } from "apollo-server-core";
+import { Prisma } from "@prisma/client";
 
 const resolvers = {
   Mutation: {
     createConversation: async (
       _: any,
-      args: { participantsIds: Array<string>; context: GraphQLContext }
-    ) => {
+      args: { participantsIds: Array<string> },
+      context: GraphQLContext
+    ): Promise<{ conversationId: string }> => {
       const { participantsIds } = args;
-      console.log("Inside create Conversation", participantsIds);
+      const { session, prisma } = context;
+
+      if (!session?.user) {
+        throw new ApolloError("Not Authorized");
+      }
+
+      const {
+        user: { id: userId },
+      } = session;
+
+      try {
+        const conversation = await prisma.conversation.create({
+          data: {
+            participants: {
+              createMany: {
+                data: participantsIds.map((id) => {
+                  return {
+                    userId: id,
+                    hasSeenLatestMessage: id === userId,
+                  };
+                }),
+              },
+            },
+            latestMessageId: null,
+          },
+          include: conversationPopulated,
+        });
+        return {
+          conversationId: conversation.id,
+        };
+      } catch (error: any) {
+        console.log("createConversation Error", error?.message);
+        throw new ApolloError("Error in creating conversation");
+      }
     },
   },
 };
 
 export default resolvers;
+
+export const participantPopulate =
+  Prisma.validator<Prisma.ConversationParticipantInclude>()({
+    user: {
+      select: {
+        id: true,
+        username: true,
+      },
+    },
+  });
+
+export const conversationPopulated =
+  Prisma.validator<Prisma.ConversationInclude>()({
+    participants: {
+      include: participantPopulate,
+    },
+    latestMessage: {
+      include: {
+        sender: {
+          select: {
+            id: true,
+            username: true,
+          },
+        },
+      },
+    },
+  });
